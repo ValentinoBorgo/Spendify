25a870557385cd4544d538ee0d73a68b4a034e09
Valentino Borgo <valentino-2000-dm@hotmail.com>
2023-10-11 09:09:13 -0300

    1.0.0

---
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.p1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnsAUtilizar();
+    }
+
+    private void btnsAUtilizar(){
+        Button btnSalir = (Button) findViewById(R.id.btnSalir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salir();
+            }
+        });
+    }
+
+    private void salir(){
+        finish();
+    }
+}
