6d90af211575cb7b408c1a46bce7562c795e38e9
Valentino Borgo <valentino-2000-dm@hotmail.com>
2023-10-14 14:41:49 -0300

    1.0.5

--- app/src/main/java/com/example/p1/MenuPrincipal.java
+++ app/src/main/java/com/example/p1/MenuPrincipal.java
@@ -1,6 +1,9 @@
 package com.example.p1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -8,12 +11,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuPrincipal extends AppCompatActivity {
 
-    TextView recibirUserView;
+    private TextView recibirUserView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        EditText campoModificar = findViewById(R.id.total);
+        campoModificar.setEnabled(false);
 
         recibirUserView = findViewById(R.id.bienvenida);
         Bundle recibirUser = getIntent().getExtras();
@@ -22,6 +27,7 @@ public class MenuPrincipal extends AppCompatActivity {
         recibirUserView.setText("Saludos " + infoUser + " !");
 
         mensaje();
+        modificarBalance();
     }
 
     private void mensaje(){
@@ -29,5 +35,23 @@ public class MenuPrincipal extends AppCompatActivity {
         int duracion = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(getApplicationContext(), mensaje, duracion);
         toast.show();
+    }
+
+    private void modificarBalance(){
+        EditText campoModificar = findViewById(R.id.total);
+        Button btnCambiarTotal = findViewById(R.id.cambiarTotal);
+        btnCambiarTotal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean editar = campoModificar.isEnabled();
+                campoModificar.setEnabled(!editar);
+
+                if(editar){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Edita tu balance actual", Toast.LENGTH_SHORT);
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "Edicion finalizada", Toast.LENGTH_SHORT);
+                }
+            }
+        });
     }
 }
