d8182af90c489d722e65228005dd80216d220e04
osvaldocozzi <meka__cozzi@hotmail.com>
2023-10-16 21:16:26 -0300

    commit clase Gasto, clase Categoria

--- app/src/main/java/com/example/p1/data/model/Gasto.java
+++ app/src/main/java/com/example/p1/data/model/Gasto.java
@@ -2,40 +2,12 @@ package com.example.p1.data.model;
 
 public class Gasto {
 
-    private int categoria1;
-    private int categoria2;
-    private int categoria3;
-    private int categoria4;
-    private int servicio;
-    private int otro;
+    private int gasto;
 
-    private int gastoTotal;
-
-    public int getGastoTotal() {
-        return gastoTotal;
+    public int getGasto() {
+        return gasto;
     }
 
-    public int getCategoria1() {
-        return categoria1;
-    }
-    public int getCategoria2() {
-        return categoria2;
-    }
-    public int getCategoria3() {
-        return categoria3;
-    }
-    public int getCategoria4() {
-        return categoria4;
-    }
-    public int getServicio() {
-        return servicio;
-    }
-    public int getOtro() { return otro;}
-
-    public void agregarGastoTotal(int gasto){ this.gastoTotal=this.gastoTotal+gasto; }
-    public void agregarGastoACategoria1(int gasto){ this.categoria1=this.categoria1+gasto; }
-    public void agregarGastoACategoria2(int gasto){ this.categoria2=this.categoria2+gasto; }
-    public void agregarGastoACategoria3(int gasto){ this.categoria3=this.categoria3+gasto; }
-    public void agregarGastoACategoria4(int gasto){ this.categoria4=this.categoria4+gasto; }
-    public void agregarGastoAServicio(int gasto){ this.servicio=this.servicio+gasto; }
+    public void actualizarGasto(int gasto){ this.gasto=this.gasto+gasto; }
+    public void setGasto(int gasto){ this.gasto=gasto;}
 }
